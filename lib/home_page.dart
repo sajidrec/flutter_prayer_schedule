3875eb78b6d9453c396f8909b996ec7a8a544fd2
MD. Sajid Hossain <sajidrec@gmail.com>
2024-01-27 21:04:30 +0600

    unnecessary comment removed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -86,7 +86,6 @@ class _HomePageState extends State<HomePage> {
                           } else {
                             screenMsg = "Something went wrong";
                             setState(() {});
-                            //TODO: implement fetching fails
                           }
                         }
                       },
